83f50d460ae214886b04ea98293e4fa899a896ed
Rodrigo Mariano <rodmariano13@gmail.com>
2021-05-25 15:51:15 -0300

    Add BDC_STAC_ITEM_IS_PUBLIC environment variable

--- bdc_stac/config.py
+++ bdc_stac/config.py
@@ -22,6 +22,7 @@ BDC_STAC_API_VERSION = _version.parse(__version__).base_version
 BDC_STAC_BASE_URL = os.getenv("BDC_STAC_BASE_URL", "http://localhost:5000")
 BDC_STAC_FILE_ROOT = os.getenv("BDC_STAC_FILE_ROOT", "http://localhost:5001")
 BDC_STAC_PNG_ROOT = os.getenv("BDC_STAC_PNG_ROOT", "http://localhost:5001")
+BDC_STAC_ITEM_IS_PUBLIC = os.getenv("BDC_STAC_ITEM_IS_PUBLIC", "None")
 BDC_STAC_MAX_LIMIT = int(os.getenv("BDC_STAC_MAX_LIMIT", "1000"))
 BDC_STAC_TITLE = os.getenv("BDC_STAC_TITLE", "Brazil Data Cube Catalog")
 BDC_STAC_ID = os.getenv("BDC_STAC_ID", "bdc")
